833f998041ed72064c0dcc11784d5c60d67859f9
tsing <tsing.goo@gmail.com>
2016-07-23 23:02:08 +0000

    Init commit

---
+++ motor.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+class Motor:
+    def __init__(self):
+        self._turn_left_pin = 7
+        self._turn_right_pin = 11
+        self._forward_pin =13
+        self._backward_pin =15
+
+        self._pins = \
+                [
+        self._turn_left_pin,
+        self._turn_right_pin,
+        self._forward_pin,
+        self._backward_pin,
+        ]
+        GPIO.setmode(GPIO.BOARD)
+        for pin in self._pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin.GPIO.LOW)
+    def dispose(self):
+        for pin in self._pins:
+            GPIO.output(pin.GPIO.LOW)
+        GPIO.cleanup()
+
+    def forward_start(self):
+        GPIO.setup(self._backward_pin, GPIO.LOW)
+        GPIO.setup(self._forward_pin, GPIO.HIGH)
+    def forward_stop(self):
+        GPIO.setup(self._forward_pin, GPIO.LOW)
+
+    def backward_start(self):
+        GPIO.setup(self._forward_pin, GPIO.LOW)
+        GPIO.setup(self._backward_pin, GPIO.HIGH)
+    def backward_stop(self):
+        GPIO.setup(self._backward_pin, GPIO.LOW)
+
+    def turn_left_start(self):
+        GPIO.setup(self._turn_right_pin, GPIO.LOW)
+        GPIO.setup(self._turn_left_pin, GPIO.HIGH)
+    def turn_left_end(self):
+        GPIO.setup(self._turn_left_pin, GPIO.LOW)
+
+    def turn_right_start(self):
+        GPIO.setup(self._turn_left_pin, GPIO.LOW)
+        GPIO.setup(self._turn_right_pin, GPIO.HIGH)
+    def turn_right_end(self):
+        GPIO.setup(self._turn_right_pin, GPIO.LOW)
+
+
+
