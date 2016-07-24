69fad21f79659b6676952bda4b30550fe7fb38a4
v3c70r <tsing.goo@gmail.com>
2016-07-24 05:30:11 +0000

    works

--- controller.py
+++ controller.py
@@ -2,29 +2,35 @@ import pygame
 from motor import Motor
 
 pygame.init()
+pygame.display.set_mode((200,200))
 _motor = Motor()
 quit = False
-while not quit:
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                _motor.forward_start()
-            if event.key == pygame.K_s:
-                _motor.backward_start()
-            if event.key == pygame.K_a:
-                _motor.turn_left_start()
-            if event.key == pygame.K_d:
-                _motor.turn_right_start()
-            if event.key == pygame.K_q:
-                quit=True
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
-                _motor.forward_stop()
-            if event.key == pygame.K_s:
-                _motor.backward_stop()
-            if event.key == pygame.K_a:
-                _motor.turn_left_end()
-            if event.key == pygame.K_d:
-                _motor.turn_right_end()
-_motor.dispose()
+try: 
+    while not quit:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    _motor.forward_start()
+                if event.key == pygame.K_s:
+                    _motor.backward_start()
+                if event.key == pygame.K_a:
+                    _motor.turn_left_start()
+                if event.key == pygame.K_d:
+                    _motor.turn_right_start()
+                if event.key == pygame.K_q:
+                    quit=True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    _motor.forward_stop()
+                if event.key == pygame.K_s:
+                    _motor.backward_stop()
+                if event.key == pygame.K_a:
+                    _motor.turn_left_end()
+                if event.key == pygame.K_d:
+                    _motor.turn_right_end()
+except KeyboardInterrupt:
+    pass
+finally:
+    _motor.dispose()
+
--- motor.py
+++ motor.py
@@ -4,10 +4,10 @@ import sys
 
 class Motor:
     def __init__(self):
-        self._turn_left_pin = 7
-        self._turn_right_pin = 11
-        self._forward_pin =13
-        self._backward_pin =15
+        self._turn_left_pin     = 15 #7
+        self._turn_right_pin    = 13 #11
+        self._forward_pin       = 11 #13
+        self._backward_pin      = 7 #15
 
         self._pins = \
                 [
@@ -19,35 +19,35 @@ class Motor:
         GPIO.setmode(GPIO.BOARD)
         for pin in self._pins:
             GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin.GPIO.LOW)
+            GPIO.output(pin,GPIO.LOW)
     def dispose(self):
         for pin in self._pins:
-            GPIO.output(pin.GPIO.LOW)
+            GPIO.output(pin, GPIO.LOW)
         GPIO.cleanup()
 
     def forward_start(self):
         GPIO.setup(self._backward_pin, GPIO.LOW)
-        GPIO.setup(self._forward_pin, GPIO.HIGH)
+        GPIO.output(self._forward_pin, GPIO.HIGH)
     def forward_stop(self):
-        GPIO.setup(self._forward_pin, GPIO.LOW)
+        GPIO.output(self._forward_pin, GPIO.LOW)
 
     def backward_start(self):
         GPIO.setup(self._forward_pin, GPIO.LOW)
-        GPIO.setup(self._backward_pin, GPIO.HIGH)
+        GPIO.output(self._backward_pin, GPIO.HIGH)
     def backward_stop(self):
-        GPIO.setup(self._backward_pin, GPIO.LOW)
+        GPIO.output(self._backward_pin, GPIO.LOW)
 
     def turn_left_start(self):
         GPIO.setup(self._turn_right_pin, GPIO.LOW)
-        GPIO.setup(self._turn_left_pin, GPIO.HIGH)
+        GPIO.output(self._turn_left_pin, GPIO.HIGH)
     def turn_left_end(self):
-        GPIO.setup(self._turn_left_pin, GPIO.LOW)
+        GPIO.output(self._turn_left_pin, GPIO.LOW)
 
     def turn_right_start(self):
         GPIO.setup(self._turn_left_pin, GPIO.LOW)
-        GPIO.setup(self._turn_right_pin, GPIO.HIGH)
+        GPIO.output(self._turn_right_pin, GPIO.HIGH)
     def turn_right_end(self):
-        GPIO.setup(self._turn_right_pin, GPIO.LOW)
+        GPIO.output(self._turn_right_pin, GPIO.LOW)
 
 
 
